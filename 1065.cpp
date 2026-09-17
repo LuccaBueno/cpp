ac021b2f9622d917ca3f2a56aafb3a16980f7db1
Lucca Bueno <107876991+LuccaBueno@users.noreply.github.com>
2026-09-17 00:32:46 -0300

    Create 1065.cpp

---
+++ 1065.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int i = 0, cont = 0;
+    
+    
+    while(i++ <5){
+    	
+    	int num;
+    	
+    	cin >> num;
+    	
+    	if(num%2==0){
+    		
+    		cont++;
+    		
+		}
+	}
+	
+	cout << cont << " valores pares" << endl;
+	
+    return 0;
+}
