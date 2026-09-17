220c4c3b24863640a870b8b8ed1fccf1e7e7ceae
Lucca Bueno <107876991+LuccaBueno@users.noreply.github.com>
2026-09-16 23:51:34 -0300

    Create 1050.cpp

---
+++ 1050.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int cod;
+    string cidade;
+    cin >> cod;
+
+switch (cod) {
+        case 61:
+            cidade = "Brasilia";
+            break;
+
+        case 71:
+            cidade = "Salvador";
+            break;
+
+        case 11:
+            cidade = "Sao Paulo";
+            break;
+
+        case 21:
+            cidade = "Rio de Janeiro";
+            break;
+
+        case 32:
+            cidade = "Juiz de Fora";
+            break;
+
+        case 19:
+            cidade = "Campinas";
+            break;
+
+        case 27:
+            cidade = "Vitoria";
+            break;
+
+        case 31:
+            cidade = "Belo Horizonte";
+            break;
+
+        default:
+            cidade = "DDD nao cadastrado";
+    }
+    
+    cout << cidade << endl;
+    
+    return 0;
+}
